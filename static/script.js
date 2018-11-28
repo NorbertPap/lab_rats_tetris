8f9d4e9720c1392ee321e104a01c7e3d4db5e5ad
NorbertPap <garrote713@gmail.com>
2018-11-28 15:28:01 +0100

    Column falling in progress

--- static/script.js
+++ static/script.js
@@ -18,18 +18,45 @@ function moveDown(board)
     function ()
     {
         let myElements = document.getElementsByClassName('cube moving');
-        let col = Number(myElements[0].dataset.col);
-        let row = Number(myElements[0].dataset.row);
+        let coordinates = [];
+        for(let i = 0; i < myElements.length; i++) {
+            let x = {col: Number(myElements[i].dataset.col),
+                        row: Number(myElements[i].dataset.row)};
+            coordinates.push(x);
 
-        if(row !== 21 && board[row+1][col] === 0)
+            // coordinates[i].col = Number(myElements[i].dataset.col);
+            // coordinates[i].row = Number(myElements[i].dataset.row);
+        }
+        coordinates.reverse();
+        let condition = true;
+        for (coordinate of coordinates) {
+            if (board[coordinate.row + 1][coordinate.col] !== 0 &&
+                !(document.querySelector(`[data-row='${coordinate.row + 1}'][data-col='${coordinate.col}'].moving`))) {
+                condition = false;
+                break;
+            }
+        }
+        if(Math.max(coordinates[0].col, coordinates[1].col, coordinates[2].col, coordinates[3].col) !== 21 && condition)
         {
-            myElements[0].style.backgroundColor='';
-            myElements[0].classList.remove('moving');
-            board[row][col] = 0;
-            board[row+1][col] = {color: 'red'};
-            let boardElement = document.querySelector(`[data-row='${row+1}'][data-col='${col}']`);
-            boardElement.style.backgroundColor = board[row+1][col].color;
-            boardElement.classList.add('moving');
+            // coordinates.sort(function (a, b) {
+            //     return b.row - a.row
+            // });
+            for (let i = 3; i >-1; i--) {
+                myElements[i].style.backgroundColor = '';
+                myElements[i].classList.remove('moving');
+                board[myElements[i].dataset.row][myElements[i].dataset.col] = 0;
+                board[myElements[i].dataset.row+1][myElements[i].dataset.col] = {color: 'red'};
+                let boardElement = document.querySelector(`[data-row='${myElements[i].dataset.row+1}'][data-col='${myElements[i].dataset.col}']`);
+                boardElement.style.backgroundColor = board[row+1][col].color;
+                boardElement.classList.add('moving');
+            }
+            // myElements[0].style.backgroundColor='';
+            // myElements[0].classList.remove('moving');
+            // board[row][col] = 0;
+            // board[row+1][col] = {color: 'red'};
+            // let boardElement = document.querySelector(`[data-row='${row+1}'][data-col='${col}']`);
+            // boardElement.style.backgroundColor = board[row+1][col].color;
+            // boardElement.classList.add('moving');
         }
         else
         {
@@ -44,9 +71,15 @@ function moveDown(board)
 function createMovingElement(board)
 {
     board[0][5] = {color: 'red'};
-    let boardElement = document.querySelector("[data-row='0'][data-col='5']");
-    boardElement.style.backgroundColor = board[0][5].color;
-    boardElement.classList.add('moving');
+    board[1][5] = {color: 'red'};
+    board[2][5] = {color: 'red'};
+    board[3][5] = {color: 'red'};
+    let boardElements = [];
+    for(let i = 0; i<4; i++){
+        boardElements.push(document.querySelector(`[data-row="${i}"][data-col='5']`));
+        boardElements[i].style.backgroundColor = board[0][5].color;
+        boardElements[i].classList.add('moving');
+    }
 }
 
 
